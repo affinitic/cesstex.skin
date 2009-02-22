eed1b8dd127b1ccee7b8c8b4367686a62d22ee46
Alain Meurant <alain.meurant@affinitic.be>
2009-02-22 22:32:37 +0000

    !!!! ca ne fonctionne pas      erruer : Cannot locate object at: http://localhost:8080/plone/front-page/document_view

--- cesstex/skin/browser/interfaces.py
+++ cesstex/skin/browser/interfaces.py
@@ -1,5 +1,6 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
-class IThemeSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer.
+class ICesstexTheme(IDefaultPloneLayer):
+    """
+    Theme for cesstex
     """
