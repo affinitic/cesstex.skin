776b49ec3a0028c20addb45e4f70c226523f0f6b
Alain Meurant <alain.meurant@affinitic.be>
2009-06-22 10:50:11 +0000

    gestion des logos des écoles selon les folders-ecoles

--- cesstex/skin/browser/interfaces.py
+++ cesstex/skin/browser/interfaces.py
@@ -1,6 +1,15 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
 
 class ICesstexTheme(IDefaultPloneLayer):
     """
     Theme for cesstex
     """
+class ILogoView(Interface):
+    """
+    Gestion des logo selon les sous-sites
+    """
+    def getLogo():
+        """
+        return the logo regarding folder
+        """
