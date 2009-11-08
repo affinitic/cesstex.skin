4d24f6ab37af6e008789bd41d0f4ded9c0acf861
Alain Meurant <alain.meurant@affinitic.be>
2009-11-08 17:49:41 +0000

    formulaire de demande d'information; envoi du mail à la direction selon l'implantation

--- cesstex/skin/browser/ManageIST.py
+++ cesstex/skin/browser/ManageIST.py
@@ -9,29 +9,63 @@ from cesstex.skin.browser.interfaces import IManageIST
 class ManageIST(BrowserView):
     implements(IManageIST)
 
-    def sendMailDemande(self):
+    def sendMailDemande(self, direction, sujet, message):
         """
         envoi de mail à la direction
         """
-        sujet="testmail"
-        message="envoi mail"
+        #yohannamazzeo@hotmail.com
+        direction="alain.meurant@affinitic.be, alain.meurant@skynet.be"
         #mailer = Mailer("localhost", "alain.meurant@skynet.be)
-        mailer = Mailer("relay.skynet.be", "alain.meurant@affinitic.be")
+        mailer = Mailer("relay.skynet.be", direction)
         mailer.setSubject(sujet)
-        mailer.setRecipients("alain.meurant@affinitic.be")
+        mailer.setRecipients("alain.meurant@affinitic.be, alain.meurant@skynet.be")
         #mailer.setRecipients("alain.meurant@skynet.be")
         mail = message
         mailer.sendAllMail(mail)
 
-    def sendMailOperateur(self, sujetOperateur, messageOperateur, adresseOperateur):
-        """
-        envoi de mail à l'operateur dont les donnees change d'état
-        """
-        mailer = Mailer("localhost", adresseOperateur)
-        #mailer = Mailer("relay.skynet.be", adresseOperateur)
-        #mailer = Mailer("smtp.scarlet.be", adresse)
-        mailer.setSubject(sujetOperateur)
-        recipients="%s, %s"%('alain.meurant@affinitic.be', adresseOperateur)
-        mailer.setRecipients(recipients)
-        mail = messageOperateur
-        mailer.sendAllMail(mail)
+    def traiterDemandeIst(self):
+        fields = self.context.REQUEST
+        civilite=fields.get('civilite')
+        prenom=fields.get('prenom')
+        nom=fields.get('nom')
+        rue=fields.get('rue')
+        numero=fields.get('numero')
+        cp=fields.get('cp')
+        localite=fields.get('localite')
+        telephone=fields.get('telephone')
+        email=fields.get('email')
+        option=fields.get('option')
+        implantation=fields.get('implantation')
+        demande=fields.get('demande')
+        
+        #pour MANAGE : direction.ist@istmanage.be   IST MANAGE
+        #pour LA LOUVIERE :  christina.papadopoulos@gmail.com  IST LA LOUVIERE
+        #pour le CEFA : ist.cefa@skynet.be  CEFA MANAGE
+        
+        if implantation=="IST MANAGE":
+            direction="direction.ist@istmanage.be"
+        elif implantation=="IST LA LOUVIERE":
+            direction="christina.papadopoulos@gmail.com"
+        elif implantation=="CEFA MANAGE":
+            direction="ist.cefa@skynet.be"
+        
+        sujet="testmail %s"%(implantation)
+        
+        message="""DEMANDE D'INFORMATION VIA LE SITE<br>
+        <hr>
+        %s %s %s<br>
+        %s, %s<br>
+        %s, %s<br>
+        %s<br>
+        %s<br>
+        <hr>
+        Option : %s<br>
+        Implantation : %s<br>
+        <hr>
+        Demande :<br>
+        %s
+        <hr>
+        pour : %s
+        """%(civilite,prenom,nom,rue,numero,cp,localite,telephone,email,option,implantation,demande,direction)
+        
+        self.sendMailDemande(direction, sujet, message)
