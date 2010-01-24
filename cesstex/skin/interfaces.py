66f34c812c4c7254bdeed04e13cc1a41901c7bd5
Alain Meurant <alain.meurant@affinitic.be>
2010-01-24 16:06:52 +0000

    essai d'ajouter un porltet collector agenda. erreur lors de gerer les portlets ##

--- cesstex/skin/interfaces.py
+++ cesstex/skin/interfaces.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+cesstex.skin
 
-#from zope.interface import Interface
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id: event.py 67630 2006-04-27 00:54:03Z jfroche $
+"""
+from zope.interface import Interface
+
+class IAgendaPortlet(Interface):
+    """
+    Interface pour le portlet agenda
+    """
