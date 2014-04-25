adbdb91219954cb37f0f163cab0739496c59b996
Alain Meurant <alain.meurant@affinitic.be>
2014-04-25 11:51:53 +0200

    ism folder view
    Info de la semaine : limitation du nombre de news à afficher

--- cesstex/skin/browser/ism_info_semaine.py
+++ cesstex/skin/browser/ism_info_semaine.py
@@ -5,20 +5,17 @@ from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 from plone.memoize.instance import memoize
 
-LIMIT = 5
-
-
 class IsmInfoSemaine(BrowserView):
 
     @memoize
-    def getNews(self):
+    def getNews(self, nombre):
         catalog = getToolByName(aq_inner(self.context), 'portal_catalog')
         return catalog(portal_type='News Item',
                        review_state=('external', 'internal'),
                        isGlobal=1,
                        sort_on='Date',
                        sort_order='reverse',
-                       sort_limit=LIMIT)[:LIMIT]
+                       sort_limit=nombre)
 
     def getNewsIconURL(self, newsBrain):
         """
