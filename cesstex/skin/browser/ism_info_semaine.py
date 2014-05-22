96763cbfe8cf5ff0dc50674fd4b8e99c829073dc
Alain Meurant <alain.meurant@affinitic.be>
2014-05-22 17:56:20 +0200

    css sur les events et les actu

--- cesstex/skin/browser/ism_info_semaine.py
+++ cesstex/skin/browser/ism_info_semaine.py
@@ -9,6 +9,9 @@ class IsmInfoSemaine(BrowserView):
 
     @memoize
     def getNews(self, nombre):
+        """
+        récupère les actualités (news)
+        """
         catalog = getToolByName(aq_inner(self.context), 'portal_catalog')
         return catalog(portal_type='News Item',
                        review_state=('external', 'internal'),
@@ -33,6 +36,9 @@ class IsmEventAgenda(BrowserView):
 
     @memoize
     def getEvents(self, nombre):
+        """
+        reécupère les événements-calendriers (events)
+        """
         catalog = getToolByName(aq_inner(self.context), 'portal_catalog')
         events = catalog(portal_type='Event',
                          review_state=('external', 'internal'),
